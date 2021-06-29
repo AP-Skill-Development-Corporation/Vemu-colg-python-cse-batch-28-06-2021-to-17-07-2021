f9fd414922b6c5750d8066a4db9efb016c5a487e
InduPriya Pokuri <34738280+InduPriya-pokuri@users.noreply.github.com>
2021-06-29 16:44:45 +0530

    Day-2 uploaded

---
+++ sample.py
@@ -0,0 +1,29 @@
+import image
+a=10
+b=20
+c=a+b
+print(a)   # 10    
+print("b")  # b
+print("c")  # c
+pint(b)     # 20
+print(c)     # 30
+print(a+b)   # 30
+print("a+b") # a+b
+
+image = pygame.image.load('1.jpg')
+
+# <img src=location.jpg>
+
+# image.show()
+
+# "This is my first addition program ......"
+# + is a one operator in python
+# It is used for additon also in strings concjddkfjkdsj
+
+
+"""This is my first addition program ......"
++ is a one operator in python
+ It is used for additon also in strings concjddkfjkdsj
+"""
+
+# import pandas
