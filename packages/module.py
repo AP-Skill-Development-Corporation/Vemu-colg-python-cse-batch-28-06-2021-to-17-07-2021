f394f994d5d64b6ab9d0d67ad38f7d811482fe3d
InduPriya Pokuri <34738280+InduPriya-pokuri@users.noreply.github.com>
2021-07-18 15:31:56 +0530

    Add files via upload

--- packages/module.py
+++ packages/module.py
@@ -1,29 +1,24 @@
-def leapyear(start,end):
-    for year in range(start,end+1):
-        if year%400==0 or (year%4==0 and year%100!=0 ):
-            print(year,end= " ")
-            
-            
-
-# duplicates[20,30,40,30,20,36,29,20,1,2,3,4,5]   =====>[1,2,3,4,5,20,30,40,36,29]
-
-def duplicates1(iteration1):
-    li=[]
-    for i in iteration1:
-        if i not in li:
-            li.append(i)
-            
-    print(li)
-            
-            
-            
-            
-            
-def evenorodd(n):
-    if n%2==0:
-        return "EVEN"
-    else:
-        return "ODD"
+class Calc:
+    def add(n1,n2):
+        return n1+n2
+    def sub(n1,n2):
+        return n1-n2
+    def mul(n1,n2):
+        return n1*n2
+    def div(n1,n2):
+        return n1/n2
+    def mod(n1,n2):
+        return n1%n2
     
     
-
+class Math:
+    def __init__(abc,val1,val2):
+        abc.val1=val1
+        abc.val2=val2
+    def show(abc):
+        print(abc.val1)
+        print(abc.val2)
+        
+    def add(abc,val3):
+        print(abc.val1+abc.val2+val3)
+    
